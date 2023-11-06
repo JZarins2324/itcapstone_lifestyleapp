59fd5fd25221faee09768d4edadcad2af833c844
amorfordcvtc <102695791+amorfordcvtc@users.noreply.github.com>
2023-11-06 11:13:04 -0600

    fist functional update

--- LifestyleConnect.php
+++ LifestyleConnect.php
@@ -1,119 +1,57 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    <header>This is used to create the database.</header>
-    <?php
-      $serverName = "localhost";
-      $username = "root";
-      $password = "mysql";
-      $dbname = "LifestyleDB";
+<?php
+$serverName = "localhost";
+$username = "root"; // It's recommended to use a username other than 'root' for applications
+$password = "mysql";
+$dbname = "LifestyleDB";
 
-      // Create Connection
-      $conn = new mysqli($serverName, $username, $password, $dbname);
-      // Check Connection
-      if ($conn->connect_error) {
-        die("Connection Failed: " . $conn->connect_error);
-      }
-      echo "Connected Successfully";
+try {
+	// Create a new PDO instance
+	$pdo = new PDO("mysql:host=$serverName;dbname=$dbname;charset=utf8mb4", $username, $password);
+	// Set the PDO error mode to exception
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// If you reach this point, the connection was successful
+	echo "Connected successfully";
+} catch (PDOException $e) {
+	// If an exception occurs, handle it here
+	echo "Connection failed: " . $e->getMessage();
+	exit; // Stop the script if the connection fails
+}
 
-      // Create Tables
-      $createLifestyleDB = "CREATE DATABASE LifestyleDB;";
-      
-      $createUsers = "CREATE TABLE users (
-          userID INT(6) AUTO_INCREMENT,
-          userName VARCHAR(16) NOT NULL,
-          userPass VARCHAR(16) NOT NULL,
-          currentTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-          PRIMARY KEY (userID)
-        );";
-        
-        $createLists = "CREATE TABLE lists (
-          listID INT(6) NOT NULL AUTO_INCREMENT,
-          listDate DATETIME NOT NULL,
-          listDesc VARCHAR(128) NOT NULL,
-          userID INT(6),
-          PRIMARY KEY (listID),
-          FOREIGN KEY (userID) REFERENCES users(userID)
-        );";
-        
-        $createPasswords = "CREATE TABLE passwords (
-          passID INT(6) NOT NULL AUTO_INCREMENT,
-          passName VARCHAR(32) NOT NULL,
-          passDesc VARCHAR(64) NOT NULL,
-          userID INT(6),
-          PRIMARY KEY (passID),
-          FOREIGN KEY (userID) REFERENCES users(userID)
-        );";
-        
-        $createNotes = "CREATE TABLE notes (
-          notesID INT(6) NOT NULL AUTO_INCREMENT,
-          notesDesc VARCHAR(256) NOT NULL,
-          userID INT(6),
-          PRIMARY KEY (notesID),
-          FOREIGN KEY (userID) REFERENCES users(userID)
-        );";
-        
-        $userData = "INSERT INTO users (userName, userPass) VALUES ('Steve', 'Captain99');";
-        $listData = "INSERT INTO lists (listDate, listDesc) VALUES ('1/1/01', 'This is an example of describing your todo');";
-        $passwordData = "INSERT INTO passwords (passName, passDesc) VALUES ('Falcon66', 'Gmail');";
-        $notesData = "INSERT INTO notes (notesDesc) VALUES ('This field has a maximum of two-hundred-fifty-six characters.');";
+// Create connection
+$conn = new mysqli($serverName, $username, $password);
 
-        if ($conn->query($createLifestyleDB) === true) {
-          echo "SQL running successfully";
-        } else {
-          echo "Error in SQL: " . $conn->error;
-        };
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-        if ($conn->query($createUsers) === true) {
-          echo "SQL running successfully";
-        } else {
-          echo "Error in SQL: " . $conn->error;
-        };
+// Create database if it doesn't exist
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully or already exists\n";
+} else {
+    echo "Error creating database: " . $conn->error;
+    $conn->close();
+    exit;
+}
 
-        if ($conn->query($createLists) === true) {
-          echo "SQL running successfully";
-        } else {
-          echo "Error in SQL: " . $conn->error;
-        };
+// Select the database
+$conn->select_db($dbname);
 
-        if ($conn->query($createPasswords) === true) {
-          echo "SQL running successfully";
-        } else {
-          echo "Error in SQL: " . $conn->error;
-        };
+// SQL to create table
+$createUsers = "CREATE TABLE IF NOT EXISTS users (
+	userID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	userName VARCHAR(30) NOT NULL,
+	userPass VARCHAR(255) NOT NULL,
+	currentTimestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
 
-        if ($conn->query($createNotes) === true) {
-          echo "SQL running successfully";
-        } else {
-          echo "Error in SQL: " . $conn->error;
-        };
 
-        if ($conn->query($userData) === true) {
-          echo "SQL running successfully";
-        } else {
-          echo "Error in SQL: " . $conn->error;
-        };
+// Check and create table if it doesn't exist
+if ($conn->query($createUsers) !== TRUE) {
+    echo "Error creating table: " . $conn->error;
+}
 
-        if ($conn->query($listData) === true) {
-          echo "SQL running successfully";
-        } else {
-          echo "Error in SQL: " . $conn->error;
-        };
-
-        if ($conn->query($passwordData) === true) {
-          echo "SQL running successfully";
-        } else {
-          echo "Error in SQL: " . $conn->error;
-        };
-
-        if ($conn->query($notesData) === true) {
-          echo "SQL running successfully";
-        } else {
-          echo "Error in SQL: " . $conn->error;
-        };
-
-        $conn->close();
-    ?>
-
-  </body>
-</html>
+// Close connection
+$conn->close();
+?>
--- server/authenticate.php
+++ server/authenticate.php
@@ -4,48 +4,54 @@
 // Start the session
 session_start();
 
-// Database connection variables
-$host = 'localhost'; // or your database host
-$dbname = 'mydatabase'; // your database name
-$user = 'dbuser'; // your database username
-$pass = 'dbpass'; // your database password
-
-// Create a new PDO instance
-$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+// Include database connection
+require_once '../LifestyleConnect.php';
 
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get username and password from form
     $username = $_POST['username'];
-    $password = $_POST['password']; // In a real application, this should be hashed!
+    $password = $_POST['password'];
 
     // Prepare a statement for execution
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE userName = ?");
     $stmt->execute([$username]);
     
     if ($stmt->rowCount() === 0) {
         // Username does not exist, create a new account
-        $createAccount = $pdo->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-        $createAccount->execute([$username, $password]);
+        // Hash the password before storing it
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $createAccount = $pdo->prepare("INSERT INTO users (userName, userPass) VALUES (?, ?)");
+        $createAccount->execute([$username, $hashedPassword]);
         
         // Set session variables
         $_SESSION['user_id'] = $pdo->lastInsertId();
         $_SESSION['username'] = $username;
         
         // Redirect to home page
-        header('Location: home.php');
+        header('Location: ../pages/home.php');
         exit();
     } else {
-        // Username exists, start the session
+        // Username exists, verify the password
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        
-        // Set session variables
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['username'] = $username;
-        
-        // Redirect to home page
-        header('Location: home.php');
-        exit();
+        if (password_verify($password, $user['userPass'])) {
+            // Set session variables
+            $_SESSION['user_id'] = $user['userID'];
+            $_SESSION['username'] = $username;
+            
+            // Regenerate session ID
+            session_regenerate_id();
+            
+            // Redirect to home page
+            header('Location: ../pages/home.php');
+            exit();
+        } else {
+            // Password is incorrect
+            // Handle the error appropriately
+            $_SESSION['error'] = 'Incorrect username or password.';
+						header('Location: ../pages/login.php');
+						exit();
+        }
     }
 }
 ?>
