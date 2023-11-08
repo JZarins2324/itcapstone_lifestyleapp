b916f4a77f7fc283482d6e8e7e5818f2f35c4fd8
Jacob Zarins <jzarins2@student.cvtc.edu>
2023-11-08 06:41:33 -0600

    Starting Input Page

---
+++ pages/input.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lifestyle Companion - Add</title>
+</head>
+<body>
+  <form action="../includes/addhandler.php" method="post">
+    <label for="new">Add new</label>
+    <select name="new" id="new">
+      <option value="note">Note</option>
+      <option value="password">Password</option>
+      <option value="todolist">To-Do List</option>
+    </select>
+    <br><br>
+
+    <label for="name">Name</label><br>
+    <input type="text" name="name" id="name" placeholder="Name"><br><br>
+
+    <label for="desc">Description</label><br>
+    <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Description..."></textarea><br><br>
+
+    <input type="submit" value="Add">
+  </form>
+</body>
+</html>
