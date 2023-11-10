ad39d2afb229ec5d663e7acac3b78786b775cfe3
Jacob Zarins <jzarins2@student.cvtc.edu>
2023-11-10 09:02:24 -0600

    basic input form css

--- pages/input.php
+++ pages/input.php
@@ -2,8 +2,8 @@
 session_start();
 
 if(!isset($_SESSION["username"])){
-    header("location: login.php");
-    exit;
+  header("location: login.php");
+  exit;
 }
 ?>
 
@@ -12,29 +12,32 @@ if(!isset($_SESSION["username"])){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="../assets/css/input.css">
   <title>Lifestyle Companion - Add</title>
 </head>
 <body>
-  <form action="../includes/addhandler.php" method="post">
-    <label for="new">Add new</label>
-    <select name="new" id="new">
-      <option value="note">Note</option>
-      <option value="password">Password</option>
-      <option value="task">Task</option>
-    </select>
-    <br><br>
+  <div id="container">
+    <form action="../includes/inputhandler.php" method="post">
+      <label for="new">Add new</label>
+      <select name="new" id="new">
+        <option value="note">Note</option>
+        <option value="password">Password</option>
+        <option value="task">Task</option>
+      </select>
+      <br><br>
 
-    <label for="name">Name</label><br>
-    <input type="text" name="name" id="name" placeholder="Name"><br><br>
+      <label for="name">Name</label><br>
+      <input type="text" name="name" id="name" placeholder="Name"><br><br>
 
-    <label for="desc">Description</label><br>
-    <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Description..."></textarea><br><br>
+      <label for="desc">Description</label><br>
+      <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Description..."></textarea><br><br>
 
-    <label id="dateLabel" for="date" style="display:none">Date</label>
-    <input type="date" name="date" id="date" style="display:none"><br><br>
+      <label id="dateLabel" for="date" style="display:none">Date</label>
+      <input type="date" name="date" id="date" style="display:none"><br><br>
 
-    <input type="submit" value="Add">
-  </form>
+      <input id="submit" type="submit" value="Add">
+    </form>
+  <div>
   <?= '<script src="../assets/js/input.js"></script>'; ?>
 </body>
 </html>
