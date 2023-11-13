2cc846bb0657a5484d129147fd02bf764ef8bbef
sschmitz <sschmitz14@student.cvtc.edu>
2023-11-13 17:14:45 -0600

    View Table Data

--- pages/view.php
+++ pages/view.php
@@ -23,13 +23,23 @@ if(!isset($_SESSION["username"])){
       while ($data = $taskData->fetch_assoc()) {
         echo "<tr><th> Task Date </th><th> Task Description </th></tr>";
         echo "<tr>";
-        echo "<td>" .$data["taskDate"]. "</td>"; 
-        echo "<td> " .$data["taskDesc"]. "</td>";
-        echo "</tr>";
+        echo "<tr><td>" .$data["taskDate"]. "</td><td> " .$data["taskDesc"]. "</td></tr>";
       }
       echo "</table>";
     }
     ?> <br> <?php
+
+    ?> <h2>Your Passwords:</h1> <?php
+
+    // Display Note Data
+    if ($passData->num_rows > 0) {
+        echo "<table>";
+        while ($data = $passData->fetch_assoc()) {
+          echo "<tr><th> Your Passwords: </th></tr>";
+          echo "<tr><td>" .$data["passName"]. "</td><td>" .$data["passDesc"]. "</td></tr>";
+        }
+        echo "</table>";
+      }
 
     ?> <h2>Your Notes:</h1> <?php
 
@@ -37,9 +47,8 @@ if(!isset($_SESSION["username"])){
     if ($noteData->num_rows > 0) {
         echo "<table>";
         while ($data = $noteData->fetch_assoc()) {
-          echo "<tr><td>";
-          echo "Note Description: " .$data["notesDesc"]. "";
-          echo "</td></tr>";
+          echo "<tr><th> Your Notes: </th></tr>";
+          echo "<tr><td>" .$data["notesDesc"]. "</td></tr>";
         }
         echo "</table>";
       }
