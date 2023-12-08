9a7182708dba2f3c3a775d623b50336c535090eb
Jacob Zarins <jzarins2@student.cvtc.edu>
2023-12-08 12:18:39 -0600

    Added Title

--- pages/input.php
+++ pages/input.php
@@ -22,7 +22,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="../assets/css/input.css">
-  <title>Document</title>
+  <title>Add Entry</title>
 </head>
 <body><?php
 
